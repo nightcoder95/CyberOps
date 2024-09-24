81858b17447bd32a72a5be0135d6ae1955f74486
Bharat Harikumar <bharathariag15@gmail.com>
2024-09-24 14:49:24 +0530

    first commit

--- Data/convert_to_json.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Load the Excel file
-df = pd.read_excel('SMM.xlsx')
-
-# Convert to JSON and ensure special characters like Malayalam letters are preserved
-df.to_json('output.json', orient='records', force_ascii=False)
